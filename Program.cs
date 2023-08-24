78150a5bf59fe13cbdf024279aada9753960f401
Laura <Laura@DESKTOP-BESSB6E>
2023-08-24 21:49:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int age = Convert.ToInt32(Console.ReadLine());
+if (age < 18)
+{
+    Console.WriteLine("Jums nepriklauso nepilnamečio akcija");
+}
+else if (age >18 && age <65)
+{
+    Console.WriteLine("Jūs esate suaugęs");
+}
+else if (age >=65)
+{
+    Console.WriteLine("Jums priklauso senioro akcija");
+}
